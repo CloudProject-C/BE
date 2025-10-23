131cd3afc63f1b15183ae5b7b79c6e30ffcb962e
다현 <123882512+dahyun24@users.noreply.github.com>
2025-10-23 23:05:41 +0900

    #1 fix: mysql 의존성 추가 및 수정

--- src/main/java/com/cloudproject/TeamC/TeamCApplication.java
+++ src/main/java/com/cloudproject/TeamC/TeamCApplication.java
@@ -2,8 +2,10 @@ package com.cloudproject.TeamC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TeamCApplication {
 
 	public static void main(String[] args) {
