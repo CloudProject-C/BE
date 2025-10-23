5fdf5a1fe74948ccac103973f3b13e933297baaf
다현 <123882512+dahyun24@users.noreply.github.com>
2025-10-23 22:35:14 +0900

    #1 chore: 프로젝트 초기 설정

---
+++ src/test/java/com/cloudproject/TeamC/TeamCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudproject.TeamC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
