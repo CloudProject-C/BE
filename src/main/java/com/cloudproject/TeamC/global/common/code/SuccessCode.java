77183d04eb68b4657b1d4edf4836d55967ab53d3
다현 <123882512+dahyun24@users.noreply.github.com>
2025-12-01 02:55:03 +0900

    #14 feat: 성공 코드 추가

--- src/main/java/com/cloudproject/TeamC/global/common/code/SuccessCode.java
+++ src/main/java/com/cloudproject/TeamC/global/common/code/SuccessCode.java
@@ -16,6 +16,10 @@ public enum SuccessCode implements BaseCode{
     FETCH_NEARBY_PLACES_SUCCESS(HttpStatus.OK, "주변 음식점 조회가 성공적으로 완료되었습니다."),
     PLACE_LIKE_TOGGLE_SUCCESS(HttpStatus.OK, "음식점 좋아요/취소가 완료되었습니다."),
     REVIEW_LIKE_TOGGLE_SUCCESS(HttpStatus.OK, "리뷰 좋아요/취소가 완료되었습니다."),
+    FETCH_MAIN_PAGE_SUCCESS(HttpStatus.OK, "메인 페이지 조회가 성공적으로 완료되었습니다."),
+    FETCH_MY_PAGE_SUCCESS(HttpStatus.OK, "마이 페이지 조회가 성공적으로 완료되었습니다."),
+    FETCH_MY_REVIEWS_SUCCESS(HttpStatus.OK, "내가 쓴 리뷰 목록 조회가 성공적으로 완료되었습니다."),
+    FETCH_MY_LIKED_PLACES_SUCCESS(HttpStatus.OK, "내가 좋아요한 음식점 목록 조회가 성공적으로 완료되었습니다.")
     ;
 
     private final HttpStatus httpStatus;
