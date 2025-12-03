0cde9473177f244072e8dc0247ba484cd7978004
grapyju <yoonju203@naver.com>
2025-12-04 03:39:15 +0900

    chore: 배포 테스트

--- src/main/java/com/cloudproject/TeamC/CampEat/application/QdrantService.java
+++ src/main/java/com/cloudproject/TeamC/CampEat/application/QdrantService.java
@@ -5,8 +5,6 @@ import com.cloudproject.TeamC.CampEat.dto.response.QdrantGetResponse;
 import com.cloudproject.TeamC.CampEat.dto.response.QdrantScrollResponse;
 import com.cloudproject.TeamC.CampEat.dto.response.QdrantSearchHit;
 import com.cloudproject.TeamC.CampEat.dto.response.QdrantSearchResponse;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.http.MediaType;
