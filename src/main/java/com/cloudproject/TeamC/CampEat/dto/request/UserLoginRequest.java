6ccb7f3a476e94b51c18d8d7f43a2c044754311e
다현 <123882512+dahyun24@users.noreply.github.com>
2025-12-07 16:16:53 +0900

    #27 feat: DTO에 swagger Schema 추가

--- src/main/java/com/cloudproject/TeamC/CampEat/dto/request/UserLoginRequest.java
+++ src/main/java/com/cloudproject/TeamC/CampEat/dto/request/UserLoginRequest.java
@@ -2,11 +2,17 @@ package com.cloudproject.TeamC.CampEat.dto.request;
 
 import lombok.*;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "로그인 요청")
 public class UserLoginRequest {
+    @Schema(description = "이메일", example = "test@khu.ac.kr")
     private String email;
+
+    @Schema(description = "비밀번호", example = "password1234")
     private String password;
 }
