1c420d55b0ee8101d80c4391b03227a2eecb8937
다현 <123882512+dahyun24@users.noreply.github.com>
2025-12-07 16:12:56 +0900

    #27 feat: 공통 success, error code 추가

--- src/main/java/com/cloudproject/TeamC/CampEat/application/AuthService.java
+++ src/main/java/com/cloudproject/TeamC/CampEat/application/AuthService.java
@@ -4,6 +4,8 @@ package com.cloudproject.TeamC.CampEat.application;
 import com.cloudproject.TeamC.CampEat.domain.School;
 import com.cloudproject.TeamC.CampEat.domain.User;
 import com.cloudproject.TeamC.CampEat.dto.request.UserJoinRequest;
+import com.cloudproject.TeamC.CampEat.exception.CampEatException;
+import com.cloudproject.TeamC.CampEat.exception.code.CampEatErrorCode;
 import com.cloudproject.TeamC.CampEat.infrastructure.repository.SchoolRepository;
 import com.cloudproject.TeamC.CampEat.infrastructure.repository.UserRepository;
 import com.cloudproject.TeamC.global.common.code.ErrorCode;
@@ -24,8 +26,12 @@ public class AuthService {
 
 
     public void registerUser(UserJoinRequest userJoinRequestDto) {
+        if (userRepository.findByEmail(userJoinRequestDto.getEmail()).isPresent()) {
+            throw new CampEatException(CampEatErrorCode.USER_ALREADY_EXISTS);
+        }
+
         School school = schoolRepository.findById(userJoinRequestDto.getSchoolId())
-                    .orElseThrow(() -> new BusinessException(ErrorCode.FORBIDDEN));
+                .orElseThrow(() -> new CampEatException(CampEatErrorCode.SCHOOL_NOT_FOUND));
 
         User user = userJoinRequestDto.toEntity(school, passwordEncoder);
         userRepository.save(user);
@@ -33,10 +39,10 @@ public class AuthService {
 
     public String login(String email, String userPassword) {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new CampEatException(CampEatErrorCode.USER_NOT_FOUND));
 
         if (!passwordEncoder.matches(userPassword, user.getPassword())) {
-            throw new RuntimeException("Invalid password");
+            throw new CampEatException(CampEatErrorCode.INVALID_PASSWORD);
         }
 
         return jwtTokenProvider.createToken(email);
--- src/main/java/com/cloudproject/TeamC/CampEat/presentation/AuthController.java
+++ src/main/java/com/cloudproject/TeamC/CampEat/presentation/AuthController.java
@@ -3,62 +3,59 @@ package com.cloudproject.TeamC.CampEat.presentation;
 
 import com.cloudproject.TeamC.CampEat.application.AuthService;
 import com.cloudproject.TeamC.CampEat.application.EmailService;
-import com.cloudproject.TeamC.CampEat.application.UserService;
 import com.cloudproject.TeamC.CampEat.dto.request.EmailVerifyRequest;
 import com.cloudproject.TeamC.CampEat.dto.request.UserJoinRequest;
 import com.cloudproject.TeamC.CampEat.dto.request.UserLoginRequest;
+import com.cloudproject.TeamC.CampEat.presentation.swagger.AuthSwagger;
 import com.cloudproject.TeamC.global.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.cloudproject.TeamC.global.common.code.ErrorCode.BAD_REQUEST;
-import static com.cloudproject.TeamC.global.common.code.SuccessCode.SEND_EMAIL_SUCCESS;
-import static com.cloudproject.TeamC.global.common.code.SuccessCode.VERIFY_EMAIL_SUCCESSS;
+import static com.cloudproject.TeamC.global.common.code.SuccessCode.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthSwagger {
 
     private final AuthService authService;
     private final EmailService emailService;
 
+    @Override
     @PostMapping("/email/send-email")
     public CommonResponse<String> sendEmail(@RequestParam String email) throws Exception {
         return CommonResponse.success(SEND_EMAIL_SUCCESS, emailService.sendMessage(email));
     }
 
+    @Override
     @PostMapping("/email/verify")
-    public CommonResponse<String> verifyCode(@RequestBody EmailVerifyRequest requestDto) {
+    public CommonResponse<Void> verifyCode(@RequestBody EmailVerifyRequest requestDto) {
         boolean check = emailService.verifyCode(requestDto);
         if (check) {
-            return CommonResponse.success(VERIFY_EMAIL_SUCCESSS,"인증 완료!");
+            return CommonResponse.success(VERIFY_EMAIL_SUCCESSS);
         }
         else {
-            return CommonResponse.failure(BAD_REQUEST, "인증 실패");
+            return CommonResponse.failure(BAD_REQUEST);
         }
     }
 
-
+    @Override
     @PostMapping("/join")
-    public ResponseEntity<String> join(@RequestBody UserJoinRequest userJoinRequestDto) {
+    public CommonResponse<Void> join(@RequestBody UserJoinRequest userJoinRequestDto) {
         authService.registerUser(userJoinRequestDto);
-        return ResponseEntity.ok("Registration successful");
+        return CommonResponse.success(USER_JOIN_SUCCESS);
     }
 
+    @Override
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UserLoginRequest userLoginDto) {
-        try {
-            String token = authService.login(userLoginDto.getEmail(), userLoginDto.getPassword());
-            return ResponseEntity.ok(token);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(401).body("Invalid password");
-        }
+    public CommonResponse<String> login(@RequestBody UserLoginRequest userLoginDto) {
+        String token = authService.login(userLoginDto.getEmail(), userLoginDto.getPassword());
+        return CommonResponse.success(USER_LOGIN_SUCCESS, token);
     }
 
 
