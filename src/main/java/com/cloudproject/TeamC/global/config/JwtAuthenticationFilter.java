bb0535c8bdc842b5971321e9cefdaca29e694051
다현 <123882512+dahyun24@users.noreply.github.com>
2025-12-16 14:57:13 +0900

    #33 fix: 불필요한 print문 제거

--- src/main/java/com/cloudproject/TeamC/global/config/JwtAuthenticationFilter.java
+++ src/main/java/com/cloudproject/TeamC/global/config/JwtAuthenticationFilter.java
@@ -7,11 +7,13 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.security.core.Authentication;
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
@@ -21,7 +23,6 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // 헤더에서 JWT 를 받아옵니다.
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
-        System.out.println("Token: " + token);
         // 유효한 토큰인지 확인합니다.
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
@@ -29,7 +30,8 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             // SecurityContext 에 Authentication 객체를 저장합니다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }else {
-            System.out.println("Invalid or no token");  // 유효하지 않은 토큰 로그 출력
+            String requestURI = ((HttpServletRequest) request).getRequestURI();
+            log.debug("유효한 JWT 토큰이 없습니다. uri: {}", requestURI);
         }
         chain.doFilter(request, response);
     }
